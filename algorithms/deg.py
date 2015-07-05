8aecdf73f6302e756c0b9d1ac971a454347e26f9
Albert Wang <albertyw@mit.edu>
2015-07-05 16:25:11 -0700

    Add deg

---
+++ algorithms/deg.py
@@ -0,0 +1,23 @@
+# Degree Array
+# http://rosalind.info/problems/deg/
+
+from collections import defaultdict
+
+
+def deg(data):
+    vertexes = defaultdict(int)
+    for x, y in data[1:]:
+        vertexes[x] += 1
+        vertexes[y] += 1
+    return [vertexes[x] for x in range(1, data[0][0]+1)]
+
+
+if __name__ == '__main__':
+    import sys
+    file_location = sys.argv[1]
+    with open(file_location, 'r') as file_handle:
+        data = file_handle.read()
+    data = data.split("\n")
+    data = [[int(y) for y in x.split(' ')] for x in data if x != '']
+    output = deg(data)
+    print(' '.join(str(x) for x in output))
