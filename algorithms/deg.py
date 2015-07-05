e9967acc04997a86ab7c82b954cf7b709281f217
Albert Wang <albertyw@mit.edu>
2015-07-05 16:43:58 -0700

    Refactor deg

--- algorithms/deg.py
+++ algorithms/deg.py
@@ -11,13 +11,16 @@ def deg(data):
         vertexes[y] += 1
     return [vertexes[x] for x in range(1, data[0][0]+1)]
 
+def parse_edge_list(data):
+    data = data.split("\n")
+    data = [[int(y) for y in x.split(' ')] for x in data if x != '']
+    return data
 
 if __name__ == '__main__':
     import sys
     file_location = sys.argv[1]
     with open(file_location, 'r') as file_handle:
         data = file_handle.read()
-    data = data.split("\n")
-    data = [[int(y) for y in x.split(' ')] for x in data if x != '']
+    data = parse_edge_list(data)
     output = deg(data)
     print(' '.join(str(x) for x in output))
